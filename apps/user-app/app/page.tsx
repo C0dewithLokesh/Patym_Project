f82a9c3613d20c8499e3d90447b27cae6a6e6a77
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-06-28 18:25:01 +0530

    recoil added

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+import { useBalance } from "@repo/store/useBalance";
 import { Button } from "@repo/ui/button";
 
 export default function Home() {
+  const value = useBalance();
   return (
-    <div className="text-red-500">Hi there
-    <Button appName="Patym" className="text-blue-500">Click</Button>
+    <div className="text-red-500">
+      Hi there {value}
+      <Button appName="Patym" className="text-blue-500">
+        Click
+      </Button>
     </div>
   );
 }
