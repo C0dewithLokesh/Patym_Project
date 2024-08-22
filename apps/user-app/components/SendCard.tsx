9052ef0a3dcd0712ab0c72c4fb61be1811eb7c2d
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-08-22 20:58:45 +0530

    send p2p money transfer done

--- apps/user-app/components/SendCard.tsx
+++ apps/user-app/components/SendCard.tsx
@@ -2,29 +2,35 @@
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 import { TextInput } from "@repo/ui/textinput";
+import { useState } from "react";
+import { p2pTransfer } from "../app/lib/actions/p2pTransfer";
 
 const SendCard = () => {
+  const [number, setNumber] = useState("");
+  const [amount, setAmount] = useState("");
+
   return (
     <Card title="Send">
       <div className="w-[300px]">
         <TextInput
           label={"Number"}
           placeholder={"Number"}
-          onChange={() => {}}
+          onChange={(value) => {
+            setNumber(value);
+          }}
         />
         <TextInput
           label={"Amount"}
           placeholder={"amount"}
           onChange={(value) => {
-            console.log(value);
+            setAmount(value);
           }}
         />
         <div className="flex justify-center pt-4">
           <Button
             onClick={async () => {
-              // if (!amount) return;
-              // await createOnRampTransaction(+amount * 100, provider);
-              // window.location.href = redirectUrl || "";
+              if (!amount) return;
+              await p2pTransfer(number, Number(amount) * 100);
             }}
           >
             Send
